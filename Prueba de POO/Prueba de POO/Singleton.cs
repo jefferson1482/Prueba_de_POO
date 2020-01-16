79b36934ed1cf109efdabb18a86d24269600f5d4
jefferson1482 <50092499+jefferson1482@users.noreply.github.com>
2020-01-16 15:00:41 -0500

    Update Singleton.cs

--- Prueba de POO/Prueba de POO/Singleton.cs
+++ Prueba de POO/Prueba de POO/Singleton.cs
@@ -8,8 +8,7 @@ namespace Prueba_de_POO
 {
     public class Singleton
     {
-        // Variable estática para la instancia, se necesita utilizar una función lambda ya que el constructor es privado
-        //static es para que el  elemento quede en memoria para que no lo elimine
+        
         public string Name { get; set; }
         private Singleton()
         {
