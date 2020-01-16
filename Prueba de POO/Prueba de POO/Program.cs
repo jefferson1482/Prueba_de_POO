3baf25bea08901fc23bccb75eff0142c1c3c7352
jefferson1482 <50092499+jefferson1482@users.noreply.github.com>
2020-01-16 14:47:24 -0500

    Add files via upload

---
+++ Prueba de POO/Prueba de POO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_de_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton1 = Singleton.GetInstance();
+            var singleton2 = Singleton.GetInstance();
+            var singleton3 = Singleton.GetInstance();
+            var singleton4 = Singleton.GetInstance();
+            singleton1.Name = "Sample 1";
+            singleton2.Name = "Sample 2";
+            singleton3.Name = "Sample 3";
+            singleton4.Name = "Sample 4";
+            Console.WriteLine("Objeto 1: " + singleton1.Name);
+            Console.WriteLine("Objeto 2: " + singleton2.Name);
+            Console.WriteLine("Objeto 3: " + singleton3.Name);
+            Console.WriteLine("Objeto 4: " + singleton4.Name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Prueba de POO/Prueba de POO/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_de_POO
+{
+    public class Singleton
+    {
+        // Variable estática para la instancia, se necesita utilizar una función lambda ya que el constructor es privado
+        //static es para que el  elemento quede en memoria para que no lo elimine
+        public string Name { get; set}
+        private Singleton()
+        {
+
+        }
+
+        private static Singleton _singleton;
+
+        public static Singleton GetInstance()
+        {
+
+            if (_singleton == null)
+            {
+
+                _singleton = new Singleton();
+
+            }
+
+            return _singleton;
+
+        }
+    }
+}
